e18a4e01429a72591561d969973b321ece6b8317
Jason Owen <jason.a.owen@gmail.com>
2012-06-17 11:10:13 -0700

    load configuration from yaml file

--- storybot.py
+++ storybot.py
@@ -1,31 +1,46 @@
 from twisted.words.protocols import irc
 from twisted.internet import protocol
+import yaml
 
 class Storybot(irc.IRCClient):
-  def _get_nickname(self):
-    return self.factory.nickname
+  def _get_username(self):
+    return self.factory.username
   def _get_password(self):
     return self.factory.password
-  nickname = property(_get_nickname)
+  def _get_nickname(self):
+    return self.factory.nickname
+  username = property(_get_username)
   password = property(_get_password)
+  nickname = property(_get_nickname)
 
   def signedOn(self):
-    self.join(self.factory.channel)
     print "Signed on as %s." % (self.nickname,)
+    for channel in self.factory.channels:
+      print "Joining %s with key %s..." % (channel["name"], channel["key"])
+      self.join(channel["name"], channel["key"])
 
   def joined(self, channel):
     print "Joined %s." % (channel,)
 
   def privmsg(self, user, channel, msg):
-    print "PRIVMSG from %s on channel %s: %s" % (user, channel, msg,)
+    if channel == self.nickname:
+      print "PRIVMSG from %s: %s" % (user, msg,)
+    else:
+      print "%s: %s said %s" % (channel, user, msg,)
+
+  def nickChanged(self, nick):
+    print "Nickname changed to %s" % (nick,)
 
 class StorybotFactory(protocol.ClientFactory):
   protocol = Storybot
 
-  def __init__(self, channel, nickname, password):
-    self.channel = channel
-    self.nickname = nickname
+  def __init__(self, username, password, operator, nickname, channels, messages):
+    self.username = username
     self.password = password
+    self.operator = operator
+    self.nickname = nickname
+    self.channels = channels
+    self.messages = messages
 
   def clientConnectionLost(self, connector, reason):
     print "Lost connection (%s), reconnecting." % (reason,)
@@ -38,8 +53,22 @@ import sys
 from twisted.internet import reactor, ssl
 
 if __name__ == "__main__":
-  nick = sys.argv[1]
-  password = sys.argv[2]
-  chan = sys.argv[3]
-  reactor.connectSSL('owenja.dyndns.org', 7000, StorybotFactory(chan, nick, password), ssl.ClientContextFactory())
+  configFile = open("config.yaml")
+  config = yaml.load(configFile)
+  configFile.close()
+
+  if config["irc"]["server"]["ssl"]:
+    reactor.connectSSL(config["irc"]["server"]["address"],
+                       config["irc"]["server"]["port"],
+                       StorybotFactory(config["irc"]["server"]["username"],
+                                       config["irc"]["server"]["password"],
+                                       config["irc"]["operator"],
+                                       config["irc"]["nickname"],
+                                       config["irc"]["channels"],
+                                       config["messages"]),
+                       ssl.ClientContextFactory())
+  else: # not ssl
+    reactor.connectTCP(config["irc"]["server"]["address"],
+                       config["irc"]["server"]["port"],
+                       StorybotFactory(config["irc"]["nick"], config["irc"]["channels"]))
   reactor.run()
