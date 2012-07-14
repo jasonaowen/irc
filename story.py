b53d61078723ecd098e8b50e0ab5c53285bb4d1f
Jason Owen <jason.a.owen@gmail.com>
2012-07-13 22:02:24 -0700

    license

--- story.py
+++ story.py
@@ -1,3 +1,22 @@
+# story.py
+# This contains all the logic for telling storys. This is what makes the bot
+# Storybot and not, say, kickbot.
+
+# Copyright 2012 Jason Owen <jason.a.owen@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from twisted.words.protocols import irc
 import random
 import datetime
